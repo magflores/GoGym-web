cd74b6a4369529d3cd7e1cc78e075b5ffe591574
slarroude <slarroude@itba.edu.ar>
2022-09-08 18:34:24 -0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>holaaa</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>404 Not found</h1>
+    <h2>Oops... Salí de aca amigo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
