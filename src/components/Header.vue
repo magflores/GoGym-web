17dce2970787fef6cea3ffba2dfc79d9371222f7
slarroude <slarroude@itba.edu.ar>
2022-09-22 00:48:53 -0300

    intento de con vue router

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app-bar app
+             color="white"
+             elevation="0"
+  ><div class="d-flex align-center">
+    <a href="/home">
+      <v-img :src="require('../assets/GoGym-icon.png')"
+             max-height="160"
+             max-width="160"
+      />
+    </a>
+  </div>
+    <h2>GoGym!</h2>
+    <v-spacer/>
+    <v-btn
+        text
+        padless color="black"
+    >
+      Join Us
+    </v-btn>
+
+    <h2
+        style="margin-right: 10px; margin-left: 10px"
+
+    >
+      |
+    </h2>
+
+    <v-btn
+        text
+        padless color="black"
+    >
+      Log In
+    </v-btn>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data: () => ({
+    user: {},
+  }),
+}
+</script>
+
+<style>
+/*v-btn{*/
+/*  font: normal normal normal 40px/50px Rambla;*/
+/*}*/
+h2 {
+  color: black;
+  font: normal normal bold 30px Rambla;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,36 @@
 <template>
-  <h1>holaaa</h1>
+  <div>
+    <v-container>
+      <v-row class="text-center" style="margin-top: 40px"  >
+        <v-col>
+          <h1 class="text--black"> Welcome to GoGym! </h1>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container fluid class="hero">
+      <v-row class="text-center" >
+        <v-col>
+          <h2 class="text--white"> Find workout routines made by others, edit them or make your own!</h2>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row class="align-center">
+        <v-col>
+          <v-btn
+              class="mx-3 white--text "
+              dark
+              icon
+          >
+            <v-icon light size="24px" >
+              mdi-tick
+            </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -9,5 +40,17 @@ export default {
 </script>
 
 <style scoped>
-
+.hero {
+  background-size: cover;
+  background: url('../assets/HomeImage.png') center center;
+  height: 650px;
+}
+h1{
+  font: normal normal bold 50px Rambla;
+  /*font-size: 50px;*/
+}
+h2 {
+  color: #FFFFFF;
+  font: normal normal normal 20px/30px Rambla;
+}
 </style>
