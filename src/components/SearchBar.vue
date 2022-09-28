81b1dc83d1cf4147456c661caab34a3ce1affbad
cami <csierra@itba.edu.ar>
2022-09-27 23:37:22 -0300

    search bar and fix in routines view

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-text-field
+
+      filled
+      label="search"
+      append-icon="mdi-magnify"
+      rounded
+      background-color="#C8C8C8"
+      dark
+      class="font"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  name: "SearchBar"
+}
+</script>
+
+<style>
+.font {
+  font-family: Rambla, sans-serif;
+  font-size: 30px;
+}
+</style>
--- src/views/RoutinesView.vue
+++ src/views/RoutinesView.vue
@@ -1,35 +1,34 @@
-<template>
+<template class="font">
   <div>
 
     <v-navigation-drawer
         clipped absolute permanent :style="{backgroundColor: variant.color}"
 
     >
-      <v-col/>
-      <v-col/>
-
-      <div class="d-flex flex-column align-center mb-6">
-        <v-row>
+      <div class="d-flex flex-column align-center mb-6" >
+        <v-row style="margin-top: 100px">
           <v-btn
               text
               depressed
               left
               x-large
+              onclick="all = true"
           >
             All Routines
           </v-btn>
         </v-row>
-        <v-row>
+        <v-row style="margin-top: 50px">
           <v-btn
               text
               depressed
               left
               x-large
+              onclick="all = false"
           >
             My Routines
           </v-btn>
         </v-row>
-        <v-row>
+        <v-row style="margin-top: 50px">
           <v-btn
               text
               depressed
@@ -49,27 +48,29 @@
           <v-row  style="margin-top: 1px;margin-left: 200px">
           <v-tabs
               background-color="#D1FD7A"
-              centered
               light
               color="black"
               grow
               hide-slider
-              align-with-title
+
           >
-            <v-tab>
-              Routines
-            </v-tab>
+            <v-tab>Routines</v-tab>
 
             <v-tab>Exercises</v-tab>
-
           </v-tabs>
           </v-row>
         </v-card>
     <v-row>
+      <div disabled="!all">
       <v-col>
 <!--        COMENTARIO: ESTO ESTA HARDCODEADO, LO CAMBIAMOS CUANDO ESTE LA API-->
-
+        <v-row style="margin-top: 20px; margin-bottom: 20px; margin-left: 350px">
+          <v-col cols="8" style="margin-left: 100px">
+          <SearchBar/>
+          </v-col>
+        </v-row>
         <v-row  style="margin-top: 20px; margin-bottom: 20px; margin-left: 270px">
+
           <v-col>
             <a href="/routinedetailed">
               <v-img :src="require('../assets/routines/routines1.png')" max-width="250px"/>
@@ -120,30 +121,33 @@
 
         </v-row>
       </v-col>
+      </div>
     </v-row>
 
   </div>
 </template>
 
 <script>
+import SearchBar from "@/components/SearchBar";
 export default {
   name: 'RoutinesView',
+  components: {SearchBar},
   data: () => ({
     user: {},
     variant: {
     color: '#D1FD7A',
     },
     expand: false,
+    all:false,
   }),
 }
 </script>
 
 <style>
-/*v-btn{*/
-/*  font: normal normal normal 40px/50px Rambla;*/
-/*}*/
-h2 {
-  color: black;
-  font: normal normal bold 30px Rambla;
+
+.font {
+font-family: Rambla, sans-serif;
+font-size: 30px;
 }
+
 </style>
